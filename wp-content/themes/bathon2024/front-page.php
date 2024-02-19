895fe668b2ac8d4ca06a14c18d7e1e683e9e93a7
ebathon <ekb67@drexel.edu>
2024-02-19 16:19:33 -0500

    Input skeleton of global header nav and footer into all pages.

--- wp-content/themes/bathon2024/front-page.php
+++ wp-content/themes/bathon2024/front-page.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php get_header(); ?>
    <h1>Front Page </h1>
    <p> This is a front-page.php template. </p>  
-</body>
-</html>
+   <?php get_footer(); ?>
